9b84b986e53ae82a8bd19ad3d1bc19b01caaa7b7
sridhar <sridhar@exura.(none)>
2013-03-05 17:46:29 +0530

    create controller and index page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emporium::Application.config.secret_token = '05fba3c7ef6eeab9e5bd888309c319a1817dcdc2936048bb757cac90513e453bd51644806083eb786347460baf5846febcb4db207279e5478ea7fa290579a85a'
